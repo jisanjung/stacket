4f3409e9d0e24a7aa87061809c8e368a8c160d55
Jisan Jung <jjjung13@gmail.com>
2022-08-18 13:21:52 -0400

    style: about page on mobile

--- src/components/welcome/About.js
+++ src/components/welcome/About.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import Heading from '../globals/Heading'
+import CharacterSVG from './CharacterSVG'
+import { BsChevronCompactDown } from "react-icons/bs";
 
 const About = () => {
   return (
-    <section>
-        <Heading text="stacket" className="text-3xl"/>
+    <section className='h-screen bg-custom-green px-8 relative'>
+        <Heading text="stacket" className="text-3xl pt-6"/>
+        <div className='h-full flex justify-center items-center'>
+            <div className='mb-56 flex flex-col justify-center'>
+                <CharacterSVG/>
+                <p className='font-bold text-2xl text-center px-8'>
+                A tool made for software engineers and developers to find jobs based on their tech stack on GitHub.
+                </p>
+            </div>
+        </div>
+        <a href="#welcomeForm" className='absolute bottom-6 left-1/2 -translate-x-1/2'>
+            <BsChevronCompactDown className='text-4xl'/>
+        </a>
     </section>
   )
 }
---
+++ src/components/welcome/CharacterSVG.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ReactComponent as PeepSVG } from "./peep59.svg";
+
+const CharacterSVG = () => {
+  return (
+    <div className='flex justify-center'>
+        <PeepSVG/>
+    </div>
+  )
+}
+
+export default CharacterSVG
