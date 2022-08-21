2ababb7a5d5001f913f7964a0ee490941e44f293
Jisan Jung <jjjung13@gmail.com>
2022-08-21 01:54:17 -0400

    feature: save user stack using github api

--- src/model.js
+++ src/model.js
@@ -3,11 +3,15 @@ import { action } from "easy-peasy";
 const globalState = {
     // state
     username: "",
+    stack: [],
 
     // actions
     setUsername: action((state, payload) => {
         state.username = payload;
-    })
+    }),
+    setStack: action((state, payload) => {
+        state.stack = payload;
+    }),
 };
 
 export default globalState;
--- src/pages/JobListings.js
+++ src/pages/JobListings.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from "react-router-dom";
+import { Octokit } from "https://cdn.skypack.dev/octokit";
+import { useStoreState, useStoreActions } from "easy-peasy";
 
 const JobListings = () => {
 
   const { user } = useParams();
+  const stack = useStoreState(state => state.stack);
+  const setStack = useStoreActions(actions => actions.setStack);
+
+  const octokit = new Octokit({ auth: process.env.REACT_APP_GITHUB_TOKEN });
+  const saveUserStack = async () => {
+    const res = await octokit.rest.repos.listForUser({
+      username: user,
+      per_page: 100,
+    });
+    setStack(res.data.map(repo => repo.language));
+  }
+
+  useEffect(() => {
+    saveUserStack();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <div>{user}</div>
+    <div>
+      {console.log(stack)}
+      {user}
+    </div>
   )
 }
 
