69d090e2f6b982fdc3c2a9a1fea5e013825f20f3
Jisan Jung <jjjung13@gmail.com>
2022-08-29 13:28:39 -0400

    logic: create function to sum object values

--- src/components/user/MobileProfileCard.js
+++ src/components/user/MobileProfileCard.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MenuCard from '../globals/MenuCard';
 import { BsChevronCompactLeft } from "react-icons/bs";
 import ProfilePic from './ProfilePic';
 import SkillMeter from './SkillMeter';
+import { useStoreState } from 'easy-peasy';
 
 const MobileProfileCard = (props) => {
+
+    const stackStats = useStoreState(state => state.stackStats);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        const sumObjectValues = obj => {
+            let sum = 0;
+            for (let key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    sum += parseInt(obj[key]);
+                }
+            }
+            return sum;
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        setTotal(sumObjectValues(stackStats))
+    }, [stackStats]);
+
   return (
     <MenuCard className="-left-10 right-1/4 bottom-0 top-0 overflow-scroll pt-6">
         <div className='flex justify-end'>
