6b0e4a7b834ead1348454416a4d8ee99b4ea1322
Jisan Jung <jjjung13@gmail.com>
2022-08-29 00:50:52 -0400

    state: octokit loading state in global state

--- src/model.js
+++ src/model.js
@@ -3,9 +3,14 @@ import { action } from "easy-peasy";
 const globalState = {
     // state
     stack: [],
-
+    octokitLoading: false,
+    
+    // actions
     setStack: action((state, payload) => {
         state.stack = payload;
+    }),
+    setOctokitLoading: action((state, payload) => {
+        state.octokitLoading = payload;
     }),
 };
 
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -8,16 +8,19 @@ import { useStoreActions } from "easy-peasy";
 const UserPage = () => {
 
     const setStack = useStoreActions(actions => actions.setStack);
+    const setOctokitLoading = useStoreActions(actions => actions.setOctokitLoading);
 
     const { user } = useParams();
     const octokit = new Octokit({ auth: process.env.REACT_APP_GITHUB_TOKEN });
     
     useEffect(() => {
+        setOctokitLoading(true)
         octokit.rest.repos.listForUser({
             username: user,
             per_page: 100,
         })
         .then(res => {
+            setOctokitLoading(false);
             const validLanguages = res.data.map(repo => repo.language).filter(language => language);
             const languageSet = new Set(validLanguages);
             setStack([...languageSet]);
