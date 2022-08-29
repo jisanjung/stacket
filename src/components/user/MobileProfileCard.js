3146a803c84b34a1eb78c4a76b0fc77a91247d49
jisanjung <jjjung13@gmail.com>
2022-08-29 14:19:38 -0400

    feature: display language stats as percentage bars

--- src/components/user/MobileProfileCard.js
+++ src/components/user/MobileProfileCard.js
@@ -1,28 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import MenuCard from '../globals/MenuCard';
 import { BsChevronCompactLeft } from "react-icons/bs";
 import ProfilePic from './ProfilePic';
-import SkillMeter from './SkillMeter';
-import { useStoreState } from 'easy-peasy';
+import Skills from './Skills';
 
 const MobileProfileCard = (props) => {
-
-    const stackStats = useStoreState(state => state.stackStats);
-    const [total, setTotal] = useState(0);
-
-    useEffect(() => {
-        const sumObjectValues = obj => {
-            let sum = 0;
-            for (let key in obj) {
-                if (obj.hasOwnProperty(key)) {
-                    sum += parseInt(obj[key]);
-                }
-            }
-            return sum;
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        setTotal(sumObjectValues(stackStats))
-    }, [stackStats]);
 
   return (
     <MenuCard className="-left-10 right-1/4 bottom-0 top-0 overflow-scroll pt-6">
@@ -34,7 +16,7 @@ const MobileProfileCard = (props) => {
         <div className='ml-10'>
             <div className='flex flex-col justify-center items-center'>
                 <ProfilePic className="w-32 h-32"/>
-                <SkillMeter percent={50}/>
+                <Skills/>
             </div>
         </div>
     </MenuCard>
---
+++ src/components/user/Skills.js
@@ -0,0 +1,38 @@
+import { useStoreState } from 'easy-peasy';
+import React, { useState, useEffect } from 'react'
+import SkillMeter from './SkillMeter';
+
+const Skills = () => {
+
+    const [total, setTotal] = useState(0);
+    const stackStats = useStoreState(state => state.stackStats);
+
+    useEffect(() => {
+        const sumObjectValues = obj => {
+            let sum = 0;
+            for (let key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    sum += parseInt(obj[key]);
+                }
+            }
+            return sum;
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        setTotal(sumObjectValues(stackStats))
+    }, [stackStats]);
+
+  return (
+    <div className='w-full'>
+        {stackStats && Object.keys(stackStats).map(language => {
+            return (
+                <div key={language}>
+                    <span>{language}</span>
+                    <SkillMeter percent={Math.trunc(parseFloat((stackStats[language] / total) * 100))}/>
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default Skills
